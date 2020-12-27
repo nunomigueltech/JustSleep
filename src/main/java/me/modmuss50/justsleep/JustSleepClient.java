e534a5cb9759cfd9bef402db32c225a5f42e8171
Nuno Rodrigues <nrodrigues0213@gmail.com>
2020-12-27 08:43:53 -0500

    1.16.4 update and rewrite

--- src/main/java/me/modmuss50/justsleep/JustSleepClient.java
+++ src/main/java/me/modmuss50/justsleep/JustSleepClient.java
@@ -1,18 +1,21 @@
 package me.modmuss50.justsleep;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.util.math.BlockPos;
 
 public class JustSleepClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ClientSidePacketRegistry.INSTANCE.register(JustSleep.SYNC_BED_STATUS, (packetContext, packetByteBuf) -> {
-            BlockPos pos = null;
+        ClientPlayNetworking.registerGlobalReceiver(JustSleep.SYNC_SPAWN_POINT, (client, networkHandler, packetByteBuf, packetSender) -> {
+            JustSleep.SpawnPoint spawnPoint = null;
             if (packetByteBuf.readBoolean()) {
-                pos = packetByteBuf.readBlockPos();
+                String dimensionId = packetByteBuf.readString();
+                BlockPos spawnPointPos = packetByteBuf.readBlockPos();
+                spawnPoint = new JustSleep.SpawnPoint(dimensionId, spawnPointPos);
             }
-            JustSleep.updateClientBedLocation(packetContext.getPlayer(), pos);
+
+            JustSleep.updateClientSpawnPoint(client.player, spawnPoint);
         });
     }
 }
--- src/main/java/me/modmuss50/justsleep/mixin/MixinGuiChatSleeping.java
+++ src/main/java/me/modmuss50/justsleep/mixin/MixinGuiChatSleeping.java
@@ -7,7 +7,9 @@ import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.SleepingChatScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,30 +30,40 @@ public abstract class MixinGuiChatSleeping extends ChatScreen {
 
 	@Inject(method = "init", at = @At("RETURN"))
 	protected void init(CallbackInfo info) {
-		button = new ButtonWidget(width / 2 - 100, height - 62, 200, 20, "Set Spawn", (widget) -> {
+		button = new ButtonWidget(width / 2 - 100, height - 62, 200, 20, new LiteralText("Set Spawn"), (widget) -> {
 			didSetSpawn = true;
 			ClientPlayNetworkHandler networkHandler = MinecraftClient.getInstance().getNetworkHandler();
 			if (networkHandler != null) {
 				networkHandler.getConnection().send(JustSleep.createSetSpawnPacket());
 			}
 		});
+		button.visible = false;
 		addButton(button);
 	}
 
 	@Override
-	public void render(int mouseX, int mouseY, float delta) {
-		super.render(mouseX, mouseY, delta);
+	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+		super.render(matrices, mouseX, mouseY, delta);
 		PlayerEntity player = MinecraftClient.getInstance().player;
-		boolean isCurrentSpawnBed = player.getSleepingPosition().orElse(BlockPos.ORIGIN).equals(JustSleep.getBedLocation(player));
-		button.visible = JustSleep.hasValidBedLocation(player) && !isCurrentSpawnBed && !didSetSpawn;
-		if (!JustSleep.hasValidBedLocation(player)) {
-			font.draw("Setting spawn point (No previous bed found)", width / 2 - 120, height - 55, Color.RED.getRGB());
+		String currentDimensionId = player.world.getRegistryKey().getValue().toString();
+
+		JustSleep.SpawnPoint spawnPoint = JustSleep.getSpawnPoint(player);
+		String spawnPointDimensionId = null;
+		BlockPos spawnPointPos = null;
+		if (spawnPoint != null) {
+			spawnPointDimensionId = spawnPoint.getDimensionId();
+			spawnPointPos = spawnPoint.getSpawnPoint();
 		}
-		if (isCurrentSpawnBed) {
-			font.draw("This is the current spawn bed", width / 2 - 80, height - 55, Color.CYAN.getRGB());
-		}
+
+		boolean isCurrentSpawnPoint = player.getSleepingPosition().orElse(BlockPos.ORIGIN).equals(spawnPointPos) &&
+				currentDimensionId.equals(spawnPointDimensionId);
+		button.visible = JustSleep.hasValidSpawnPoint(player) && !isCurrentSpawnPoint && !didSetSpawn;
 		if (didSetSpawn) {
-			font.draw("Spawn point updated to this bed", width / 2 - 80, height - 55, Color.GREEN.getRGB());
+			this.textRenderer.draw(matrices, "Spawn point updated to this bed", width / 2 - 80, height - 55, Color.GREEN.getRGB());
+		} else if (isCurrentSpawnPoint) {
+			this.textRenderer.draw(matrices, "This is the current spawn bed", width / 2 - 80, height - 55, Color.CYAN.getRGB());
+		} else if (!JustSleep.hasValidSpawnPoint(player)) {
+			this.textRenderer.draw(matrices, "Setting spawn point (No previous bed found)", width / 2 - 120, height - 55, Color.RED.getRGB());
 		}
 	}
 
